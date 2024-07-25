06feb3b1a63d8eee5dd7c8d0cc62aa05097e1c8f
tonori <tonori@tonori.cn>
2024-07-25 19:17:17 +0800

    feat: 支持 metadata 和 filters 参数

--- tools/add_memory.py
+++ tools/add_memory.py
@@ -1,4 +1,5 @@
 import httpx
+import json
 
 from core.tools.tool.builtin_tool import BuiltinTool
 from core.tools.entities.tool_entities import ToolInvokeMessage
@@ -20,13 +21,21 @@ class AddMem0AIMemory(BuiltinTool):
             timeout=60
         )
 
+        _metadata = {}
+
+        if tool_parameters.get("data") == "":
+            raise ValueError("No data provided")
+
+        if tool_parameters.get("metadata", None):
+            _metadata = json.loads(tool_parameters.get("metadata"))
+
         response = http_client.post("/store", json={
             "data": tool_parameters.get("data"),
             "user_id": tool_parameters.get("user_id", user_id),
             "agent_id": tool_parameters.get("agent_id"),
             "run_id": tool_parameters.get("run_id"),
-            "metadata": tool_parameters.get("metadata"),
-            "filters": tool_parameters.get("filters"),
+            "metadata": _metadata,
+            "filters": tool_parameters.get("filters") or {},
             "prompt": tool_parameters.get("prompt")
         })
 
--- tools/query_memory.py
+++ tools/query_memory.py
@@ -1,4 +1,5 @@
 import httpx
+import json
 
 from core.tools.tool.builtin_tool import BuiltinTool
 from core.tools.entities.tool_entities import ToolInvokeMessage
@@ -20,12 +21,18 @@ class QueryMem0AIMemory(BuiltinTool):
             timeout=60
         )
 
-        response = http_client.get("/search", params={
+        _filters = None
+
+        if tool_parameters.get("filters", None) is not None:
+            _filters = json.loads(tool_parameters.get("filters"))
+
+        response = http_client.post("/search", json={
             "query": tool_parameters.get("query"),
             "user_id": tool_parameters.get("user_id", user_id),
             "agent_id": tool_parameters.get("agent_id"),
             "run_id": tool_parameters.get("run_id"),
             "limit": tool_parameters.get("limit"),
+            "filters": _filters
         })
 
         return self.create_text_message(response.text)
